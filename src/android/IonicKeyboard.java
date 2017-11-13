02df72f2b3d5c8e4de1a1e0086f7ed42050009ad
Venka Tesh G <gvenkatesh225@gmail.com>
2017-11-13 14:35:25 +0530

    Update IonicKeyboard.java

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -110,8 +110,8 @@ public class IonicKeyboard extends CordovaPlugin {
                             } else {
                                 screenHeight = rootViewHeight;
                             }
-                            // Dialog dialog = new Dialog(rootView);
-                            Dialog dialog = cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content);
+                             Dialog dialog = new Dialog(cordova.getActivity().getApplicationContext());
+                           // Dialog dialog = cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content);
                             dialog.setCanceledOnTouchOutside(true);
                             int heightDiff = screenHeight - resultBottom;
 
